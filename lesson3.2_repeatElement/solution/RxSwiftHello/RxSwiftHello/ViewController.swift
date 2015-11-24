635eb0fe1a3b2e0d60ff471f6c6bca267d0a2409
Jason Pepas <jasonpepas@gmail.com>
2015-11-24 01:07:18 -0600

    adding lesson 3.2

---
+++ lesson3.2_repeatElement/solution/RxSwiftHello/RxSwiftHello/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  RxSwiftHello
+//
+//  Created by Pepas Personal on 11/24/15.
+//  Copyright © 2015 Pepas Labs. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class InfiniteHelloGenerator
+{
+    class func generate() -> Observable<String>
+    {
+        return RxSwift.repeatElement("hello", MainScheduler.sharedInstance)
+    }
+}
+
+class ViewController: UIViewController {
+
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        InfiniteHelloGenerator.generate().subscribeNext { (s) -> Void in
+            debugPrint(s)
+        }.addDisposableTo(disposeBag)
+    }
+}
